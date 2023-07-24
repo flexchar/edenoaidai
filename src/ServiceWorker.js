17a126ef52b109ddf277d10e0fd1549d2c9a88f0
dariu <darius.eic@gmail.com>
2023-07-24 16:53:54 +0300

    Added database.js to precaching in ServiceWorker.js

--- src/ServiceWorker.js
+++ src/ServiceWorker.js
@@ -7,7 +7,7 @@
 const { strategies, expiration, cacheableResponse } = workbox;
 
 // Use the CACHE_VERSION constant
-const CACHE_VERSION = 'v3';
+const CACHE_VERSION = 'v4';
 
 /**
  * Forcefully take over clients after update
@@ -46,6 +46,9 @@ workbox.precaching.precacheAndRoute([
     },
     {
         url: './views/Single.vue',
+    },
+    {
+        url: './database.js',
     },
     ...self.__precacheManifest /* eslint-disable-line */,
 ]);
